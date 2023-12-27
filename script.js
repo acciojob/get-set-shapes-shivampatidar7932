f669185212e1f4975191abf0c2ee25387efc707c
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-27 20:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate and return the area of the rectangle
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Animal {
+	constructor(side) {
+    // Calling the constructor of the superclass (Rectangle) with the same side for width and height
+    super(side, side);
+  }
+
+  // Method to calculate and return the perimeter of the square
+  getPerimeter() {
+    // The perimeter of a square is the sum of all four sides
+    return 4 * this.width;
+  }
+}
+const rectangle = new Rectangle(5, 10);
+console.log( rectangle.getArea());
+
+const square = new Square(4);
+console.log( square.getArea());
+console.log( square.getPerimeter());
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
